21aab3b5e2ad03abffe06e4ead719dcf90e44caa
Maysam Hafezparast <maysam@192.168.1.10>
2017-12-15 20:55:40 +0800

    verygoodla

---
+++ www/js/app.js
@@ -0,0 +1,17 @@
+
+var app = angular.module('starter', ['ionic'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
